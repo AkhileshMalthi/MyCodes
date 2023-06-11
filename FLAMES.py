6ce9264ff22087ca53d44206cdc03bd4701b49b5
Akhilesh Malthi <119729949+AkhileshMalthi@users.noreply.github.com>
2023-06-11 10:10:48 +0530

    Add files via upload
    Just a Simple Code for FLAMES

---
+++ FLAMES.py
@@ -0,0 +1,25 @@
+#A python program to get the Flames of two numbers
+def numofuncommons():
+    num = len(name1+name2)
+    for i in range(len(name1)):
+        for j in range(len(name2)):
+            if name1[i]==name2[j]:
+                num -= 2
+                break
+    return num
+
+def result(steps):
+    flames = ["Friends", "Love", "Attraction", "Marriage", "Enemy", "Siblings"]
+    ind = 0
+    for i in range(5):
+        ind = (ind+steps-1)%(6-i)
+        flames.pop(ind)
+    print(*flames)
+
+name1 = list(input("Enter First name: ").casefold())
+while(" " in name1):
+    name1.remove(" ")
+name2 = list(input("Enter Second name: ").casefold())
+while(" " in name1):
+    name2.remove(" ")
+result(numofuncommons())
