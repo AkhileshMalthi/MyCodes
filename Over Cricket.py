265f70cb014419884e18f33811a4c97039a142a1
Akhilesh Malthi <119729949+AkhileshMalthi@users.noreply.github.com>
2023-12-05 17:38:02 +0530

    Update Over Cricket.py
    Some validations and handlings were made

--- Over Cricket.py
+++ Over Cricket.py
@@ -1,69 +1,66 @@
 import random
 
-def play_cricket():
-    # Greetings
-    print("Welcome to OVER CRICKET GAME")
+def get_valid_input(message):
+    while True:
+        try:
+            user_input = int(input(message))
+            if user_input not in range(1, 7):
+                raise ValueError
+            return user_input
+        except ValueError:
+            print("Please enter a valid number between 1 and 6.")
 
-    # Setting defaults
+def play_innings(batting_player, bowling_player):
     scores = [1, 2, 3, 4, 6]
-    com_score = 0
-    player_score = 0
+    score = 0
 
-    # Getting player details
+    for over in range(6):
+        bowler = random.choice(scores)
+        batsman = get_valid_input(f"{batting_player}, Hit --> ")
+
+        if batsman == 5:
+            batsman = 4
+        elif batsman > 6:
+            batsman = 6
+
+        print(f"{bowling_player} delivered {bowler}")
+        if bowler == batsman:
+            print("OUT")
+            break
+        else:
+            score += batsman
+            print(f"Score: {score}".rjust(50))
+
+    return score
+
+def play_cricket():
+    print("Welcome to OVER CRICKET GAME")
     player_name = input("Enter Your Name: ")
 
-    # 1st Innings Code
-    print("1st Innings".center(50))
-    print("-".center(50, "-"))
-    print(f"\nBatting : {player_name}\nBowling : Computer\n")
-    print("-".center(50, "-"))
+    overs_per_inning = int(input("Enter number of overs per inning: "))
 
-    # Player's batting
-    for i in range(6):
-        com = random.choice(scores)
-        player = int(input("Hit --> "))
+    player_score = 0
+    computer_score = 0
 
-        # Restricting the hits as per rules
-        if player == 5:
-            player = 4
-        elif player > 6:
-            player = 6
+    for inning in range(1, 3):
+        print(f"{inning}st Innings".center(50))
+        print("-".center(50, "-"))
+        print(f"\nBatting: {'Computer' if inning == 1 else player_name}\n"
+              f"Bowling: {player_name if inning == 1 else 'Computer'}\n")
+        print("-".center(50, "-"))
 
-        print(f"Computer delivered for {com}")
-        if com == player:
-            print("OUT")
-            break
+        if inning == 1:
+            player_score = play_innings(player_name, "Computer")
+            print(f"2nd Innings".center(50), f"\nTarget --> {player_score + 1}")
+            print("-".center(50, "-"))
         else:
-            player_score += player
-            print(f"Score: {player_score}".rjust(50))
+            computer_score = play_innings("Computer", player_name)
 
-    # 2nd Innings Code
-    print("2nd Innings".center(50), f"\nTarget --> {player_score + 1}")
-    print("-".center(50, "-"))
-    print(f"\nBatting : Computer\nBowling : {player_name}\n")
-    print("-".center(50, "-"))
-
-    # Computer's batting
-    for i in range(6):
-        player = int(input("Hit --> "))
-        com = random.choice(scores)
-        if com == player:
-            print("OUT")
-            break
-        elif com_score > player_score:
-            break
-        else:
-            print(f"Computer HIT --> {com} <--")
-            com_score += com
-            print(f"Score: {com_score}".rjust(50))
-            print(f"Need {player_score - com_score} to Win")
-
-    # Game result
-    print("-".center(50, "-"))
-    print("Game Over".center(50))
-    print(f"{player_name} Won".center(50) if player_score > com_score else
-          "Computer Won".center(50) if player_score < com_score else "DRAW".center(50))
-    print("-".center(50, "-"))
+        print("-".center(50, "-"))
+        print("Game Over".center(50))
+        print(f"{player_name} Won".center(50) if player_score > computer_score else
+              "Computer Won".center(50) if player_score < computer_score else "DRAW".center(50))
+        print("-".center(50, "-"))
 
 if __name__ == '__main__':
     play_cricket()
