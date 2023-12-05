97bc4fa5090dc0c4a45f43526ea65addc1911083
Akhilesh Malthi <119729949+AkhileshMalthi@users.noreply.github.com>
2023-12-05 17:33:50 +0530

    Update Flames.py
    small changes and added comments

--- Flames.py
+++ Flames.py
@@ -1,12 +1,10 @@
-"""
-        FLAMES PROGRAM BY AKHILESH MALTHI
-"""
 import time
 
-# function which returns the number of letters remaining after common letters are eliminated
-def numofuncommons(name1,name2): 
-
-    popped = [] # to store the eliminated letters
+def num_of_uncommons(name1, name2):
+    """
+    Returns the number of letters remaining after eliminating common letters between two names.
+    """
+    popped = []  # Store eliminated letters
 
     for i in range(len(name1)):
         for j in range(len(name2)):
@@ -16,34 +14,42 @@ def numofuncommons(name1,name2):
                 time.sleep(0.5)
                 break
 
-    rem = (len(name1)+len(name2))-len(popped)*2
-    print(f"Total number of LETTERS REMAINING --> {rem}")
-    return rem
+    remaining_letters = (len(name1) + len(name2)) - len(popped) * 2
+    print(f"Total number of LETTERS REMAINING --> {remaining_letters}")
+    return remaining_letters
 
 def relation(steps):
+    """
+    Determines the relationship based on the number of steps.
+    """
     flames = ["Friends", "Love", "Attraction", "Marriage", "Enemy", "Siblings"]
     ind = 0
 
     for i in range(5):
-        ind = (ind+steps-1)%(6-i)
+        ind = (ind + steps - 1) % (6 - i)
         flames.pop(ind)
     print(*flames)
 
-def result():
-    relation(numofuncommons(name1,name2))
-        
 def get_details():
-        name1 = input("Enter First name: ").casefold()
-        name1 = "".join(name1.split())
-        
-        name2 = input("Enter Second name: ").casefold()
-        name2 = "".join(name2.split())
-        
-        # for faster result i will choose the smaller name as name1 and the otherone as name2
-        if len(name1) > len(name2):
-            name1,name2 = name2,name1
-        return name1,name2
+    """
+    Gets input for two names, removes spaces and chooses the smaller name as name1.
+    """
+    name1 = input("Enter First name: ").casefold()
+    name1 = "".join(name1.split())
+
+    name2 = input("Enter Second name: ").casefold()
+    name2 = "".join(name2.split())
+
+    if len(name1) > len(name2):
+        name1, name2 = name2, name1
+    return name1, name2
+
+def result():
+    """
+    Gets details and calculates the relationship.
+    """
+    name1, name2 = get_details()
+    relation(num_of_uncommons(name1, name2))
 
 if __name__ == '__main__':
-        name1,name2 = get_details()
-        result()
+    result()
